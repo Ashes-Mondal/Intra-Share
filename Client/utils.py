cac1a6fed8327915696f56c44411dd7698c286fd
Ashes Mondal <imt_2019021@iiitm.ac.in>
2022-01-25 08:09:10 +0530

    end-to-end encryption implemented

--- Client/utils.py
+++ Client/utils.py
@@ -3,10 +3,14 @@ import os
 import pickle
 import sys
 import threading
-from queue import Queue
-
 import tkinter as tk
+from queue import Queue
 from tkinter import filedialog
+
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+import base64
 
 
 def encodeJSON(input: dict):
@@ -89,3 +93,121 @@ def getAppLastState(username,server_addr):
                 except Exception as e:
                     break
     return nList
+
+#!<--------------- Hybrid encryption (RSA + AES) ------------->!#
+def generate_AES_key(size = 16):
+    return get_random_bytes(size)
+
+def generate_RSA_keys(username: str,size = 1024):
+    key = RSA.generate(size)
+    pubkey = key.publickey().export_key()
+    privkey = key.export_key("PEM")
+    
+    folder_path = os.path.join(f'keys/{username}')
+    publickey_path = os.path.join(folder_path,"public.pem")
+    privatekey_path = os.path.join(folder_path,"private.pem")
+    
+    if os.path.exists(folder_path) == False:
+        os.makedirs(folder_path)
+
+    
+    ##writing keys into PEM files
+    with open(publickey_path,mode='wb') as f:
+        f.write(pubkey)
+        
+    with open(privatekey_path,mode='wb') as f:
+        f.write(privkey)
+    return pubkey,privkey
+
+def get_keys(username:str):
+    ##check if keys exists
+    folder_path = os.path.join(f'keys/{username}')
+    publickey_path = os.path.join(folder_path,"public.pem")
+    privatekey_path = os.path.join(folder_path,"private.pem")
+    
+    if os.path.exists(folder_path) == False:
+        return None,None
+    
+    ##reading keys from PEM files
+    with open(publickey_path,mode='rb') as f:
+        pubkey = f.read()
+        
+    with open(privatekey_path,mode='rb') as f:
+        privkey = f.read()
+    return pubkey,privkey
+
+#*********** Encryption **************#
+def encrypt_AES_key(pubkey: bytes,aeskey):
+    rsakey = RSA.import_key(pubkey)
+    rsacipher = PKCS1_OAEP.new(rsakey)
+    encrypted_aeskey = rsacipher.encrypt(aeskey)
+    return encrypted_aeskey
+
+def encrypt_message(pubkey,message:str):
+    #Todo:returns bundle (e_aeskey,e_msg,tag,nonce)
+    ##Generate aeskey and encrypt using recipient's pubkey
+    aeskey = generate_AES_key()
+    e_aeskey = encrypt_AES_key(pubkey, aeskey)
+    
+    ##Encrypt the message with above aeskey
+    message = message.encode('utf-8')
+    aescipher = AES.new(aeskey, AES.MODE_EAX)
+    e_msg,tag = aescipher.encrypt_and_digest(message)
+    nonce = aescipher.nonce
+    
+    ##encoding to base64 and then decode
+    e_aeskey = base64.b64encode(e_aeskey).decode()
+    e_msg = base64.b64encode(e_msg).decode()
+    tag = base64.b64encode(tag).decode()
+    nonce = base64.b64encode(nonce).decode()
+    
+    return (e_aeskey ,e_msg, tag, nonce)
+
+#*********** decryption **************#
+def decrypt_AES_key(username: str,e_aeskey):
+    publickey,privatekey = get_keys(username=username)
+    privkey = RSA.import_key(privatekey)
+    rsacipher = PKCS1_OAEP.new(privkey)
+    aeskey = rsacipher.decrypt(e_aeskey)
+    return aeskey
+
+def decrypt_message(username: str,bundle: list):
+    e_aeskey,e_msg,tag,nonce = bundle
+    
+    ##encode and then decode
+    e_aeskey = base64.b64decode(e_aeskey.encode())
+    e_msg = base64.b64decode(e_msg.encode())
+    tag = base64.b64decode(tag.encode())
+    nonce = base64.b64decode(nonce.encode())
+    
+    aeskey = decrypt_AES_key(username,e_aeskey)
+    aescipher = AES.new(aeskey, AES.MODE_EAX,nonce)
+    msg = aescipher.decrypt_and_verify(e_msg, tag)
+    return msg.decode()
+#!<--------------- ****************** ------------->!#
+
+if __name__=='__main__':
+    username = "Ashes"
+    ##sender
+    message = "Hello world"
+    
+    #*encrypting aeskey with receiver's pubkey
+    pubkey,privkey = get_keys(username)
+    if privkey == None:
+        pubkey,privkey = generate_RSA_keys(username)
+        
+    #*encrypting message with aeskey
+    e_aeskey,e_msg,tag,nonce = encrypt_message(pubkey, message)
+    request = {
+            "type": "send_message",
+            "data": {
+                "sender": 1,
+                "receiver": 2,
+                "bundle": [e_aeskey,e_msg,tag,nonce]
+            }
+        }
+    res = encodeJSON(request)
+    res = json.loads(res)
+    ##receiver
+    msg = decrypt_message(username,res["data"]["bundle"])
+    print(msg)
